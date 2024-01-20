ea826a47974f54a2afd6d7383770ce081a999022
DanielPaes <daniel.p.aes@hotmail.com>
2024-01-20 14:31:11 -0300

    Adicionado arquivo de configuração bd

--- repositories/db.js
+++ repositories/db.js
@@ -1,12 +1,17 @@
 import pg from "pg";
+import { readFile } from "fs/promises";
+
+const apiConfig = JSON.parse(await readFile("./config/config.json"));
+const uri = apiConfig.uri;
 
 async function connect() {
+  console.log(process.cwd());
   if (global.connection) {
     return global.connection.connect();
   }
+
   const pool = new pg.Pool({
-    connectionString:
-      "",
+    connectionString: uri,
   });
   global.connection = pool;
 
